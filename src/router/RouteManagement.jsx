142c7dcadbfc8c1de1eb2313939d6d31b0ecb752
Cantika Firalya <tikafiralya@gmail.com>
2023-05-19 04:16:54 +0700

    vite default

---
+++ src/router/RouteManagement.jsx
@@ -0,0 +1,44 @@
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import LayoutComponent from "../components/layouts/LayoutComponent";
+import LoadingComponent from "../components/loadingComponent/LoadingComponent";
+
+
+import HomePage from "../pages/homePage/HomePage";
+
+
+const RouteManagement = () => {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      navigate("/");
+    }
+  }, [token]);
+
+  return (
+    <Suspense fallback={<LoadingComponent />}>
+
+        <LayoutComponent>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            {/* <Route path="/form" element={<FormComponent />} />
+            <Route path="/form-exp" element={<FormComponentExp />} />
+            <Route path="/form-crud" element={<FormCRUD />} />
+            <Route path="/form-crud-api" element={<FormCRUDAPI />} />
+            <Route path="/form-crud-exp" element={<FormCRUD_graph />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/about-me" element={<AboutMe />} />
+            <Route path="/about-me/:id" element={<AboutMe />} />
+            <Route path="/product" element={<ProductPage />} />
+            <Route path="/product/:uuid" element={<ProductDetail />} />
+            <Route path="/content" element={<Contentful />} /> */}
+          </Routes>
+        </LayoutComponent>
+      
+    </Suspense>
+  );
+};
+
+export default RouteManagement;
