6acf2ebed4490ef0d72af5538cbf7f4b810ef9cd
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-05-20 21:46:39 +0700

    menyelesaikan contact section

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -5,17 +5,27 @@ import { CancelButton } from "../../components/buttonComponent/ButtonComponent";
 import "./homePage.css";
 import Gap from "../../components/gap/Gap";
 import { menu, service } from "./constant";
+import { GoogleButton, Logo } from "../../assets/index";
+import {
+  PhoneOutlined,
+  MailOutlined,
+  FacebookOutlined,
+  InstagramOutlined,
+  YoutubeOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
 
 const HomePage = () => {
-
   // radio button
-  const [section, setSection] = useState('food')
+  const [section, setSection] = useState("food");
 
   return (
     <div>
-
       <section className="SERVICE">
-        <h2 className="titleS">Our Special Service <br />from Nusantara Meals</h2>
+        <h2 className="titleS">
+          Our Special Service <br />
+          from Nusantara Meals
+        </h2>
         <div>
           <Row>
             {service.map((item) => (
@@ -33,15 +43,22 @@ const HomePage = () => {
 
       <section className="MENU">
         <h2 className="titleM">This Month Hottest Menu</h2>
-        <p className="descM">the best of Indonesia's culinary treasures, all in one place</p>
+        <p className="descM">
+          the best of Indonesia's culinary treasures, all in one place
+        </p>
         <div>
           <Row>
             <Radio.Group
-              defaultValue='food'
+              defaultValue="food"
               buttonStyle="solid"
-              value={section}>
-              <Radio.Button className="food" value='food' style={{ zIndex: 0 }}>Food's</Radio.Button>
-              <Radio.Button className="drink" value='drink'>Drink's</Radio.Button>
+              value={section}
+            >
+              <Radio.Button className="food" value="food" style={{ zIndex: 0 }}>
+                Food's
+              </Radio.Button>
+              <Radio.Button className="drink" value="drink">
+                Drink's
+              </Radio.Button>
             </Radio.Group>
           </Row>
         </div>
@@ -50,10 +67,10 @@ const HomePage = () => {
           <Row gutter={15}>
             {menu.map((item) => (
               <Col span={6}>
-                <Card className="cardM"
+                <Card
+                  className="cardM"
                   cover={<img className="imgM" alt="example" src={item.foto} />}
                 >
-
                   <p className="judulM">{item.judul}</p>
                   <p className="kotaM">{item.kota}</p>
                   <div className="hargakalor">
@@ -67,6 +84,142 @@ const HomePage = () => {
         </div>
       </section>
 
+      <section className="CONTACT">
+        <Row justify="center" align="middle">
+          <Col
+            xs={24}
+            sm={24}
+            md={20}
+            lg={20}
+            xl={20}
+            className="cont-container"
+          >
+            <Space direction="horizontal">
+              <Row gutter={12} justify="space-between" className="">
+                <Col span={18}>
+                  <Space direction="vertical" className="container-col-contact">
+                    <img src={Logo} alt="logo" className="contact-nusa" />
+                    <span className="text-experience">
+                      Experience the best of Nusantara cuisine at your
+                      fingertips.
+                    </span>
+                    <span className="cont-us">Contact Us</span>
+                    <div className="divider-cont-us" />
+                    <Row justify="start" align="middle">
+                      <div className="icon-container">
+                        <PhoneOutlined
+                          style={{
+                            fontSize: "25px",
+                            transform: "rotate(90deg)",
+                          }}
+                        />
+                      </div>
+                      <span className="phone-mail">+628592617712</span>
+                    </Row>
+                    <Row justify="start" align="middle">
+                      <div className="icon-container">
+                        <MailOutlined
+                          style={{
+                            fontSize: "25px",
+                          }}
+                        />
+                      </div>
+                      <span className="phone-mail">halo@nusameal.com</span>
+                    </Row>
+                  </Space>
+                </Col>
+                <Col span={6}>
+                  <Space direction="vertical" className="container-col-contact">
+                    <Button className="btn-google" />
+                    <Row gutter={12}>
+                      <Col span={10}>
+                        <ul style={{ paddingLeft: "0px" }}>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Home
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Service
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Menu
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Contact
+                            </Button>
+                          </li>
+                        </ul>
+                      </Col>
+                      <Col span={14}>
+                        <ul style={{ paddingLeft: "0px" }}>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Terms and Conditions
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Support
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Button className="btn-list" type="text">
+                              Privacy Policy
+                            </Button>
+                          </li>
+                          <li className="list-btn-contact">
+                            <Space
+                              wrap
+                              direction="horizontal"
+                              className="container-btn-social"
+                            >
+                              <Button
+                              className="btn-social"
+                                icon={
+                                  <FacebookOutlined className="icon-social" />
+                                }
+                              />
+
+                              <Button
+                              className="btn-social"
+                                icon={
+                                  <InstagramOutlined className="icon-social" />
+                                }
+                              />
+                              <Button
+                              className="btn-social"
+                                icon={
+                                  <TwitterOutlined className="icon-social" />
+                                }
+                              />
+                              <Button
+                              className="btn-social"
+                                icon={
+                                  <YoutubeOutlined className="icon-social" />
+                                }
+                              />
+                              <Button
+                              className="btn-social"
+                                icon={<PhoneOutlined className="icon-social" />}
+                              />
+                            </Space>
+                          </li>
+                        </ul>
+                      </Col>
+                    </Row>
+                  </Space>
+                </Col>
+              </Row>
+            </Space>
+          </Col>
+        </Row>
+      </section>
     </div>
   );
 };
