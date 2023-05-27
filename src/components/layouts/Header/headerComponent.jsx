08fa6b211774ddc035e016af734b94dd7fa45ed6
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-05-27 12:34:53 +0700

    revisi header responsive transparent bg color

--- src/components/layouts/Header/headerComponent.jsx
+++ src/components/layouts/Header/headerComponent.jsx
@@ -1,22 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./header.css";
 import { Layout, Menu, Row, Button, Collapse } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import { MENU_ITEM } from "../constants";
 import { Link } from "react-router-dom";
 import { Logo } from "../../../assets";
+import classnames from "classnames";
 
 const HeaderComponent = () => {
   const { Header } = Layout;
   const path = window.location.pathname;
   const [current, setCurrent] = useState(path);
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const onClick = (e) => {
     setCurrent(e.key);
   };
 
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Header className="head-container">
-      <Row justify="space-between">
+    <Header className="head-container" style={{ 
+      background:  isScrolled ? "#fff" : "transparent",
+     }}>
+      <Row justify="space-between" >
         <Link to="/" className="logoHidden">
           <Row className="brand">
             <img
@@ -37,6 +58,9 @@ const HeaderComponent = () => {
           onClick={onClick}
           selectedKeys={[current]}
           className="mobileHidden"
+          style={{ 
+            backgroundColor:  isScrolled ? "#fff" : "transparent",
+           }}
           disabledOverflow
         />
 
