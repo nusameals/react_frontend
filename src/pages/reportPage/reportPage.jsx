a7ec5b23cf4802ef5e281037270776f7b3fb8ee4
Cantika Firalya <tikafiralya@gmail.com>
2023-06-12 19:26:28 +0700

    Update reportPage.jsx

--- src/pages/reportPage/reportPage.jsx
+++ src/pages/reportPage/reportPage.jsx
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Breadcrumb, Button, Form, Card, Col, Row, Space } from "antd";
-import {
-
-  Table,
-  Modal,
-  Popconfirm, Pagination
- 
-} from "antd";
+import { Divider, Input, Table, Modal, Popconfirm, Pagination } from "antd";
 
 import axios from "axios";
 
@@ -39,12 +33,18 @@ export const ReportPage = () => {
     }
   };
 
-  
+  const [searchedText, setSearchedText] = useState("");
+
   const columns = [
     {
       title: "ID",
       dataIndex: "idReservation",
       key: "idReservation",
+
+      //       onFilter: (value, record)  => {
+      // return record.name. includes (value);
+      //       },
+
       render: (_, record) => (
         <div
           style={{
@@ -54,20 +54,36 @@ export const ReportPage = () => {
           {record.idReservation}
         </div>
       ),
-    },  
+    },
     {
-        title: "Years",
-        dataIndex: "years",
-        key: "years",
-        sorter: (a, b) => a.years - b.years,
-        sortDirections: ["ascend", "descend"],  
-      },
+      title: "Years",
+      dataIndex: "years",
+      key: "years",
+      sorter: (a, b) => a.years - b.years,
+      sortDirections: ["ascend", "descend"],
+    },
     {
       title: "Month",
       dataIndex: "month",
       key: "month",
       sorter: (a, b) => a.month.localeCompare(b.month),
       sortDirections: ["ascend", "descend"],
+      filteredValue: [searchedText],
+      onFilter: (value, record) => {
+        return (
+          String(record.idReservation)
+            .toLowerCase()
+            .includes(value.toLowerCase()) ||
+          String(record.years).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.month).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.cash).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.debitCard)
+            .toLowerCase()
+            .includes(value.toLowerCase()) ||
+          String(record.ewallet).toLowerCase().includes(value.toLowerCase()) ||
+          String(record.total).toLowerCase().includes(value.toLowerCase())
+        );
+      },
     },
     {
       title: "Cash",
@@ -86,11 +102,10 @@ export const ReportPage = () => {
       key: "ewallet",
     },
     {
-        title: "Total",
-        dataIndex: "total",
-        key: "total",
-      }
-    
+      title: "Total",
+      dataIndex: "total",
+      key: "total",
+    },
   ];
   return (
     <div>
@@ -108,31 +123,73 @@ export const ReportPage = () => {
         />
         <span className="text-profile">Report Data</span>
       </Row>
-      <Table
+      <Card
+        bordered={false}
         style={{
-          margin: "50px 60px",
+          margin: "2% 2%",
         }}
-        dataSource={dataSource}
-        columns={columns}
-        pagination={paginationConfig}
+      >
+        <div
+          style={{
+            display: "flex",
+            gap: 10,
+            alignItems: "center",
+            // margin: "10px 60px",
+            // marginBottom: "-20px",
+          }}
+        >
+          <span style={{ fontSize: 14 }}>Search:</span>
+          <Input.Search
+            placeholder="Please enter"
+            style={{
+              width: 500,
+            }}
+            onSearch={(value) => {
+              setSearchedText(value);
+            }}
+            onChange={(e) => {
+              setSearchedText(e.target.value);
+            }}
+          />
+        </div>
+        {/* <div style={{ backgroundColor: "blue", 
+      }}>
+        <hr
+          style={{
+            margin: "2% -2%",
+            color: "#fafafa",
+            backgroundColor: "#fafafa", 
 
-      />
-  <Space>
-    <Card
-      size="small"
-      title="Payment"
-      bordered={false}
-      extra={<p>ⓘ</p>}
-      style={{
-        width: 300,
-      }}
-    >
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-  </Space>    </div>
-
+          }}
+        />
+        </div> */}
+        <Divider></Divider>
+        <Table
+          style={{
+            margin: "2% 0%",
+          }}
+          dataSource={dataSource}
+          columns={columns}
+          pagination={paginationConfig}
+        />
+      </Card>
+      <Space>
+        <Card
+          // size="small"
+          // title="Payment"
+          // bordered={false}
+          // extra={<p>ⓘ</p>}
+          style={{
+            width: 300,
+          }}
+        >
+          <p>Payments</p>
+          <p>ⓘ</p>
+          <p>Card content</p>
+          <p>Card content</p>
+        </Card>
+      </Space>{" "}
+    </div>
   );
 };
 export default ReportPage;
