a58b78b4555b13c948969e8ea25f22911e17d446
alwanjauza <92279555+alwanjauza@users.noreply.github.com>
2023-06-14 21:36:46 +0700

    add dummy logout button

--- src/pages/adminPage/AdminPage.jsx
+++ src/pages/adminPage/AdminPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import { Button } from "antd";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const AdminPage = () => {
-    return (
-        <div>
-            <h1>ADMIN PAGE</h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>ADMIN PAGE</h1>
+      <Link to="/">
+        <Button
+          type="primary"
+          onClick={() => {
+            localStorage.removeItem("token");
+            localStorage.removeItem("username");
+            localStorage.removeItem("id");
+          }}
+          danger
+        >
+          Logout
+        </Button>
+      </Link>
+    </div>
+  );
+};
 
 export default AdminPage;
