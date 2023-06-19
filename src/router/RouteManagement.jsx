9685746203e51ef1f6041b86d56c2c92da314c00
alwanjauza <92279555+alwanjauza@users.noreply.github.com>
2023-06-19 10:01:13 +0700

    update routemanagement

--- src/router/RouteManagement.jsx
+++ src/router/RouteManagement.jsx
@@ -6,8 +6,8 @@ import LoadingComponent from "../components/loadingComponent/LoadingComponent";
 import HomePage from "../pages/homePage/HomePage";
 import ProfilePage from "../pages/profilePage/ProfilePage";
 import EditProfile from "../pages/profilePage/editProfile/EditProfile";
-import MenuPage from '../pages/menuPage/Create Menu/createMenu';
-import EditMenu from '../pages/menuPage/Edit Menu/editMenu';
+import MenuPage from "../pages/menuPage/Create Menu/createMenu";
+import EditMenu from "../pages/menuPage/Edit Menu/editMenu";
 import OrderPage from "../pages/orderPage/orderPage";
 import ReservationsPage from "../pages/reservationsPage/reservationsPage";
 import ReportPage from "../pages/reportPage/reportPage";
@@ -20,8 +20,8 @@ const RouteManagement = () => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
   const pathName = window.location.pathname;
-  
-   return (
+
+  return (
     <Suspense fallback={<LoadingComponent />}>
       {!token && pathName === "/" ? (
         <LayoutComponent>
@@ -32,18 +32,18 @@ const RouteManagement = () => {
       ) : !token && pathName === "/admin" ? (
         <Routes>
           <Route path="/admin" element={<LoginPage />} />
-  <Route path="/profile-setting" element={<ProfilePage />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path='/menu-page' element={<MenuPage />} />
-          <Route path='/edit-menu' element={<EditMenu />} />
-          <Route path="/order-page" element={<OrderPage />} />
-          <Route path="/reservations-page" element={<ReservationsPage />} />
-          <Route path="/report-page" element={<ReportPage />} />
-          <Route path="/add-table" element={<AddTable />} /
         </Routes>
       ) : (
         <Routes>
           <Route path="/dashboard" element={<AdminPage />} />
+          <Route path="/profile-setting" element={<ProfilePage />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/menu-page" element={<MenuPage />} />
+          <Route path="/edit-menu" element={<EditMenu />} />
+          <Route path="/order-page" element={<OrderPage />} />
+          <Route path="/reservations-page" element={<ReservationsPage />} />
+          <Route path="/report-page" element={<ReportPage />} />
+          <Route path="/add-table" element={<AddTable />} />
         </Routes>
       )}
     </Suspense>
