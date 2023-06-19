33dc44443f71b486000757a906a21b75cedf98f1
Cantika Firalya <tikafiralya@gmail.com>
2023-06-19 15:48:53 +0700

    dayjs

--- src/pages/reservationsPage/reservationsPage.jsx
+++ src/pages/reservationsPage/reservationsPage.jsx
@@ -7,15 +7,18 @@ import {
   Radio,
   InputNumber,
   Table,
-  Modal,  
+  Modal,
   Popconfirm,
   Upload,
-  Divider, Tabs
+  Divider,
+  Tabs,
 } from "antd";
-import { CloseSquareFilled } from '@ant-design/icons';
+import { CloseSquareFilled } from "@ant-design/icons";
 
 import axios from "axios";
 import "./reservationsPage.css";
+import dayjs from 'dayjs';
+
 
 export const ReservationsPage = () => {
   const [dataSource, setDataSource] = useState([]);
@@ -35,9 +38,8 @@ export const ReservationsPage = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  const handleShowModal = () => {
-    // Modified handleShowModal function
-    // setSelectedId(id);
+  const handleShowModal = (id) => {
+    setSelectedId(id);
     setVisible(true);
   };
   const [searchedText, setSearchedText] = useState("");
@@ -97,13 +99,13 @@ export const ReservationsPage = () => {
       title: "Customer Name",
       dataIndex: "customerName",
       key: "customerName",
-      width: 192.67,
+      width: 200,
     },
     {
       title: "Phone",
       dataIndex: "phone",
       key: "phone",
-      width: 192.67,
+      width: 200,
     },
     {
       title: "Date",
@@ -112,37 +114,56 @@ export const ReservationsPage = () => {
       width: 120,
       sorter: (a, b) => new Date(a.date) - new Date(b.date),
       sortDirections: ["ascend", "descend"],
+      render: (date) => dayjs(date).format("DD-MM-YYYY"),
+
     },
     {
       title: "Time In",
       dataIndex: "timeIn",
       key: "timeIn",
       width: 120,
-      sorter: (a, b) => new Date(a.date) - new Date(b.date),
       sortDirections: ["ascend", "descend"],
+
+            // render: (date) => dayjs(date).format(   "HH:mm"),
+
+            sorter: (a, b) => {
+              const timeA = dayjs(a.timeIn, "HH:mm");
+              const timeB = dayjs(b.timeIn, "HH:mm");
+              return timeA - timeB;
+            },
+            render: (time) => dayjs(time, "HH:mm").format("HH:mm"),
+              // render: (timestamp) => dayjs(timestamp).format("HH:mm"),
+
     },
     {
       title: "Time Out",
       dataIndex: "timeOut",
       key: "timeOut",
       width: 120,
-      sorter: (a, b) => new Date(a.date) - new Date(b.date),
       sortDirections: ["ascend", "descend"],
+      sorter: (a, b) => {
+        const timeA = dayjs(a.timeIn, "HH:mm");
+        const timeB = dayjs(b.timeIn, "HH:mm");
+        return timeA - timeB;
+      },
+      render: (time) => dayjs(time, "HH:mm").format("HH:mm"),
     },
     {
       title: "Action",
       key: "action",
-      width: 102,
+      width: 142,
       render: (text, record) => {
         return (
           <Space size="middle">
-            <a onClick={handleShowModal}>View Details</a>
+            <a onClick={() => handleShowModal(record.idReservation)}>
+              View Details
+            </a>
           </Space>
         );
       },
     },
   ];
-  
+
   const columnsTable = [
     {
       title: "Image",
@@ -156,7 +177,7 @@ export const ReservationsPage = () => {
           alt={`avatar-${index}`}
           style={{ height: "30px" }}
         />
-      )
+      ),
     },
     {
       title: "Number of Tables",
@@ -167,13 +188,10 @@ export const ReservationsPage = () => {
       filteredValue: [searchedText],
       onFilter: (value, record) => {
         return (
-
           String(record.numberofTables)
             .toLowerCase()
             .includes(value.toLowerCase()) ||
-          String(record.seats)
-            .toLowerCase()
-            .includes(value.toLowerCase()) ||
+          String(record.seats).toLowerCase().includes(value.toLowerCase()) ||
           String(record.type).toLowerCase().includes(value.toLowerCase())
         );
       },
@@ -183,14 +201,12 @@ export const ReservationsPage = () => {
       dataIndex: "seats",
       key: "seats",
       width: 259.33,
-
     },
     {
       title: "Type",
       dataIndex: "type",
       key: "type",
       width: 259.33,
-
     },
     {
       title: "Action",
@@ -211,190 +227,179 @@ export const ReservationsPage = () => {
   const [activeTab, setActiveTab] = useState("Data");
 
   const items = [
-
     {
       key: "Data",
       label: `Data`,
-      children:      
-      <div className="childcard" style={{backgroundColor: "#fafafa"}}>
-      <Card
-        bordered={false}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          margin: "2%",
-
-          marginLeft: "auto",
-          marginRight: "auto",
-          width: "1250px",
-          
-
-        }}
-      >
-        <div
-          style={{
-            gap: 10,
-            alignItems: "center",
-            display: "flex",
-          }}
-        >
-          <span style={{ fontSize: 14 }}>Search:</span>
-          <Input
-            placeholder="Please enter"
+      children: (
+        <div className="childcard" style={{ backgroundColor: "#fafafa" }}>
+          <Card
+            bordered={false}
             style={{
-              width: 500,
+              display: "flex",
+              alignItems: "center",
+              margin: "2%",
+
+              marginLeft: "auto",
+              marginRight: "auto",
+              width: "1250px",
             }}
-            onSearch={(value) => {
-              setSearchedText(value);
-            }}
-            onChange={(e) => {
-              setSearchedText(e.target.value);
-            }}
-          />
+          >
+            <div
+              style={{
+                gap: 10,
+                alignItems: "center",
+                display: "flex",
+              }}
+            >
+              <span style={{ fontSize: 14 }}>Search:</span>
+              <Input
+                placeholder="Please enter"
+                style={{
+                  width: 500,
+                }}
+                onSearch={(value) => {
+                  setSearchedText(value);
+                }}
+                onChange={(e) => {
+                  setSearchedText(e.target.value);
+                }}
+              />
+            </div>
+            <Table
+              style={{
+                margin: "1% 0%",
+              }}
+              dataSource={dataSource}
+              columns={columnsData.map((column) => ({
+                ...column,
+                // title: <span style={{ fontWeight: "normal" }}>{column.title}</span>,
+              }))}
+              pagination={paginationConfig}
+            />
+          </Card>
         </div>
-        <Table
-          style={{
-            margin: "1% 0%",
-            
-          }}
-          dataSource={dataSource}
-          columns={columnsData.map((column) => ({
-            ...column,
-            // title: <span style={{ fontWeight: "normal" }}>{column.title}</span>,
-          }))}
-          pagination={paginationConfig}
-        />
-      </Card>
-      </div>
+      ),
     },
     {
       key: "Table List",
       label: `Table List`,
       //buat testing aja blom rapi
-      children:       
-      <div className="childcard" style={{backgroundColor: "#fafafa"}}>
-
-      </div>
+      children: (
+        <div className="childcard" style={{ backgroundColor: "#fafafa" }}></div>
+      ),
     },
   ];
-// modal biar bisa keluar sesuai id
+  // modal biar bisa keluar sesuai id
 
-const [rowData, setRowData] = useState(dataSource);
-const handleClose = () => {
-  // Set the visibility to false to close the modal
-  setVisible(false);
-};
+  const [rowData, setRowData] = useState(dataSource);
+  const handleClose = () => {
+    // Set the visibility to false to close the modal
+    setVisible(false);
+  };
 
   const handleTabChange = (key) => {
     setActiveTabKey(key);
   };
-  
+
   return (
     <div>
       {/* {" "} */}
-      <Row className="container-header-profile" >
+      <Row className="container-header-profile">
         <div className="reserheader">
-        <Breadcrumb
-          items={[
-            {
-              title: "Reservations",
-            },
-            {
-              title: "Reservations Data",
-            },
-          ]}
-        />
-        <span className="text-profile">Reservations Data</span>
-        </div>
-
-      </Row>
-      <Row justify="space-between" >
-          <Tabs
-            defaultActiveKey="Data"
-            onChange={handleTabChange}
-            
-            items={items}  
-            tabBarStyle={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'flex-start',
-              padding: '0px 24px',
-              backgroundColor: '#ffffff',
-              
-marginBottom:'-0.1%'
-            }}
-            style={{ width: '100%' }}
+          <Breadcrumb
+            items={[
+              {
+                title: "Reservations",
+              },
+              {
+                title: "Reservations Data",
+              },
+            ]}
           />
-        </Row>
+          <span className="text-profile">Reservations Data</span>
+        </div>
+      </Row>
+      <Row justify="space-between">
+        <Tabs
+          defaultActiveKey="Data"
+          onChange={handleTabChange}
+          items={items}
+          tabBarStyle={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            padding: "0px 24px",
+            backgroundColor: "#ffffff",
 
-      <Modal visible={visible}
+            marginBottom: "-0.1%",
+          }}
+          style={{ width: "100%" }}
+        />
+      </Row>
 
-              closeIcon={<CloseSquareFilled style={{ color: 'red', fontSize: 20 }} />}
-              onCancel={handleClose}  
-              footer={[
-                <div style={{ display: 'flex', justifyContent: 'center' }}>
-
-                <Button key="submit" type="primary" onClick={handleClose}
-                style={{ width:"100px",backgroundColor: "#0669BD" }}>
-                  Submit
-                </Button>
-                </div>
-]}        >
-        <div className="modalheader">
-          <p className="titlemodalreser">
-            <b>Reservations Status</b>
+      <Modal
+        visible={visible}
+        closeIcon={<CloseSquareFilled style={{ color: "red", fontSize: 20 }} />}
+        onCancel={handleClose}
+        footer={[
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button
+              key="submit"
+              type="primary"
+              onClick={handleClose}
+              style={{ width: "100px", backgroundColor: "#0669BD" }}
+            >
+              Submit
+            </Button>
+          </div>,
+        ]}
+      >
+        <div>
+          <div className="modalheader">
+            <p className="titlemodal">
+              <b>Reservations Status</b>
+            </p>
+            <p className="subtitle">
+              Here, you can see the reservations details
+            </p>
+          </div>
+          <Divider style={{ marginTop: "-8px" }} />
+          <p className="titledetail">
+            <b>Reservations Detail</b>
           </p>
-          <p className="subtitle">Here, you can see the Reservations details</p>
+          <div
+            style={{ display: "flex", justifyContent: "space-between" }}
+          ></div>
         </div>
-        <Divider style={{ marginTop: '-8px' }}/>
-        <p className="titledetail">
-          <b>Reservations Detail</b>
-        </p>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div className="modalisian">
-            <p className="subdetail">Customer Username</p>
-            <p className="subdetail">Order Number</p>
-            <p className="subdetail">Type</p>
-            <p className="subdetail">Table Number</p>
-          </div>
 
-          <div className="modalrespon">
-            <p className="subrespon">
-              <b>{rowData?.idReservation}</b>
-            </p>
-            <p className="subrespon">
-            </p>
-            <p className="subrespon">
-              <b>Dine In</b>
-            </p>
-            <p className="subrespon">
-              <b>4</b>
-            </p>
-          </div>
-          {/*                    
-                    {orders?.map((row_id) => (
-                        <div key={row_id} className='modalrespon'>
-                            <p className='subrespon'><b>{row_id.customerUsername}</b></p>
-                            <p className='subrespon'><b></b></p>
-                            <p className='subrespon'><b>Dine In</b></p>
-                            <p className='subrespon' ><b>4</b></p>
-                        </div>
-                    ))} */}
-        </div>
-        {/* <p>y</p>
-        {dataSource.map((record) => (
-          <div key={record.idReservation}>
-            <p>ID: {record.idReservation}</p>
-            <p>Customer Username: {record.customerUsername}</p>
-            <p>Customer Name: {record.customerName}</p>
-            <p>Phone: {record.phone}</p>
-            <p>Date: {record.date}</p>
-            <p>Time In: {record.timeIn}</p>
-            <p>Time Out: {record.timeOut}</p>
-            <Divider />
-          </div>
-        ))} */}
-        
+        {dataSource
+          .filter((record) => record.idReservation === selectedId)
+          .map((record) => (
+            <div key={record.idReservation}>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <div className="modalisian">
+                  <p className="subdetail">Name</p>
+                  <p className="subdetail">Number Phone</p>
+                  <p className="subdetail">Date</p>
+                  <p className="subdetail">Start - End</p>
+                  <p className="subdetail">Agenda</p>
+                  <p className="subdetail">Number of people</p>
+                </div>
+                <div className="modalrespon">
+                  <p className="subrespon">
+                    <b>{record.customerName}</b>
+                  </p>
+                  <p className="subrespon"><b> {record.phone}</b></p>
+                  <p className="subrespon"><b>{record.date}</b></p>
+                  <p className="subrespon"> <b>{record.phone}</b></p>
+                  <p className="subrespon"> {record.date}</p>
+                  <p>Time In: {record.timeIn}</p>
+                  <p>Time Out: {record.timeOut}</p>
+                  <Divider />
+                </div>{" "}
+              </div>
+            </div>
+          ))}
       </Modal>
     </div>
   );
