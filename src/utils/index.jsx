c99605af8a56aaa3aafac91fb0a4f89e49cd2c5e
alwan.20030@mhs.unesa.ac.id <alwan.20030@mhs.unesa.ac.id>
2023-06-25 21:57:24 +0700

    update API login

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,5 +1,5 @@
 export const BASE_URL = {
-  API: `${import.meta.env.VITE_APP_BASE_URL}`,
+  API: `${import.meta.env.VITE_APP_BASE_URL2}`,
 };
 export const BASE_URL2 = {
   API: `${import.meta.env.VITE_APP_BASE_URL2}`,
