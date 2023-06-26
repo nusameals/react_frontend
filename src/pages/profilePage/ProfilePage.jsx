399d09cefc55cf0cbe09fce740c2a847305c63e6
Andi Wicaksono Nugroho <75154388+Andiwn121@users.noreply.github.com>
2023-06-26 22:00:03 +0700

    bismillah aman ya ges dev2 lagi

--- src/pages/profilePage/ProfilePage.jsx
+++ src/pages/profilePage/ProfilePage.jsx
@@ -23,56 +23,47 @@ const ProfilePage = () => {
       </Row>
 
       <Row justify="center" align="middle" className="container-profile-card">
-        <Card className="card-profile">
-          <Button
-            type="text"
-            icon={<ArrowLeftOutlined style={{ padding: "0px" }} />}
-            className="btn-back-profile"
-          >
-            Back
-          </Button>
-          <Row className="row-content-profile">
-            <Col className="col-text-profile">
-              <Row justify="start" className="name-profile">
-                <span className="text-name">Name</span>
-                <span className="text-profile-name">Ahmad</span>
-              </Row>
-              <Row className="role-profile">
-                <Col>
-                  <span className="text-name">Role</span>
-                  <span className="text-profile-name">Admin</span>
-                </Col>
-                <Col offset={1}>
+          <Card className="card-profile">
+            <Button
+              type="text"
+              icon={<ArrowLeftOutlined style={{ padding: "0px" }} />}
+              className="btn-back-profile"
+            >
+              Back
+            </Button>
+            <Row className="row-content-profile">
+              <Col className="col-text-profile">
+                <Row className="name-profile">
+                  <span className="text-name">Username</span>
+                  <span className="text-profile-name">{localStorage.getItem("username")}
+                  </span>
+                </Row>
+                <Row className="name-profile">
                   <span className="text-name">Gender</span>
                   <span className="text-profile-name">Male</span>
-                </Col>
-              </Row>
-              <Row className="name-profile">
-                <span className="text-name">Username</span>
-                <span className="text-profile-name">Ahmad321</span>
-              </Row>
-              <Row className="name-profile">
-                <span className="text-name">Phone</span>
-                <span className="text-profile-name">081234567890</span>
-              </Row>
-            </Col>
-            <Col className="col-img-profile">
-              <Row>
-                <Space direction="vertical">
-                  <img
-                    src={mangga}
-                    alt="avatar-profile"
-                    className="avatar-profile"
-                  />
-                  <Link to="/edit-profile">
-                    <Button className="btn-edit-profile">Edit Profile</Button>
-                  </Link>
-                </Space>
-              </Row>
-            </Col>
-          </Row>
-        </Card>
-      </Row>
+                </Row>
+                <Row className="name-profile">
+                  <span className="text-name">Phone</span>
+                  <span className="text-profile-name">081234567890</span>
+                </Row>
+              </Col>
+              <Col className="col-img-profile">
+                <Row>
+                  <Space direction="vertical">
+                    <img
+                      src={mangga}
+                      alt="avatar-profile"
+                      className="avatar-profile"
+                    />
+                    <Link to="/edit-profile">
+                      <Button className="btn-edit-profile">Edit Profile</Button>
+                    </Link>
+                  </Space>
+                </Row>
+              </Col>
+            </Row>
+          </Card>
+        </Row>
     </>
   );
 };
--- src/pages/profilePage/editProfile/EditProfile.jsx
+++ src/pages/profilePage/editProfile/EditProfile.jsx
@@ -10,9 +10,11 @@ import {
   Radio,
   Row,
   Space,
+  Upload,
 } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
 import { mangga } from "../../../assets";
+import { Link } from "react-router-dom";
 
 const EditProfile = () => {
   const layout = {
@@ -23,6 +25,7 @@ const EditProfile = () => {
       span: 19,
     },
   };
+
 
   return (
     <>
@@ -44,69 +47,74 @@ const EditProfile = () => {
       </Row>
 
       <Row justify="center" align="middle" className="container-profile-card">
-        <Card className="card-profile">
-          <Row className="row-content-profile" justify="center" align="middle">
-            <Col>
-              <Form {...layout} className="container-edit-form">
-                <Form.Item label="Name" name="name">
-                  <Input style={{ width: "320px" }} />
-                </Form.Item>
+          <Card className="card-edit-profile">
+            <Form
+            {...layout}
+              className="container-edit-form"
+            >
+              <Row className="row-content-edit" justify="center" align="middle">
+                <Col>
+                  <Form.Item label="Username" name="username">
+                    <Input
+                      style={{ width: "320px" }}
+                    ></Input>
+                  </Form.Item>
 
-                <Form.Item label="Role" name="role">
-                  <Radio.Group>
-                    <Space direction="horizontal">
-                      <Radio value={"Admin"}>Admin</Radio>
-                      <Radio value={"Cashier"}>Cashier</Radio>
-                    </Space>
-                  </Radio.Group>
-                </Form.Item>
+                  <Form.Item label="Gender" name="gender">
+                    <Radio.Group
+                    >
+                      <Space direction="horizontal">
+                        <Radio value={"Male"}>Male</Radio>
+                        <Radio value={"Female"}>Female</Radio>
+                      </Space>
+                    </Radio.Group>
+                  </Form.Item>
 
-                <Form.Item label="Gender" name="gender">
-                  <Radio.Group>
-                    <Space direction="horizontal">
-                      <Radio value={"Male"}>Male</Radio>
-                      <Radio value={"Female"}>Female</Radio>
-                    </Space>
-                  </Radio.Group>
-                </Form.Item>
+                  <Form.Item
+                    label="Phone"
+                    name="phone"
+                    rules={[
+                      {
+                        pattern: /^\d{11,12}$/,
+                        message: "Phone number is not valid!",
+                      },
+                    ]}
+                  >
+                    <Input
+                      style={{ width: "320px" }}
+                    />
+                  </Form.Item>
+                  <Space className="container-btn-edit-profile">
+                    <Button type="primary" htmlType="submit">
+                      Save
+                    </Button>
+                    <Link to="/profile-setting">
+                      <Button type="danger">Cancel</Button>
+                    </Link>
+                  </Space>
+                </Col>
 
-                <Form.Item label="Username" name="username">
-                  <Input style={{ width: "320px" }} />
-                </Form.Item>
-
-                <Form.Item
-                  label="Phone"
-                  name="phone"
-                  rules={[
-                    {
-                      pattern: /^\d{11}$/,
-                      message: "Phone number is not valid!",
-                    },
-                  ]}
-                >
-                  <Input style={{ width: "320px" }} />
-                </Form.Item>
-                <Space className="container-btn-edit-profile">
-                  <Button type="primary" htmlType="submit">
-                    Save
-                  </Button>
-                  <Button type="danger">Cancel</Button>
-                </Space>
-              </Form>
-            </Col>
-
-            <Col className="col-img-profile">
-              <Row>
-                <img
-                  src={mangga}
-                  alt="avatar-profile"
-                  className="avatar-profile"
-                />
+                <Col className="col-img-profile">
+                  <Row>
+                    <Form.Item>
+                      <Upload
+                        showUploadList={false}
+                        name="file"
+                        maxCount={1}
+                      >
+                          <Button
+                            className="btn-edit-admin-ava"
+                            style={{ width:"252px" }}
+                            icon={<img src={mangga} alt="avatar-profile" className="avatar-profile"/>}
+                          />
+                      </Upload>
+                    </Form.Item>
+                  </Row>
+                </Col>
               </Row>
-            </Col>
-          </Row>
-        </Card>
-      </Row>
+            </Form>
+          </Card>
+        </Row>
     </>
   );
 };
