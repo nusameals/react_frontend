4e56269eb06d90c32d94b7c68acafce621700c4b
Cantika Firalya <tikafiralya@gmail.com>
2023-06-27 17:31:49 +0700

    pretier reservation

--- src/pages/reservationsPage/Constant.jsx
+++ src/pages/reservationsPage/Constant.jsx
@@ -1,19 +1,20 @@
 export const INITIAL_TABLE_DATA = [
-    {
-      key: 1,
-      image: "https://photoshopinspire.com/wp-content/uploads/2011/08/avatar_reloaded_by_saswat777.jpg",
-      name: "Belatung",
-      city: "Jember",
-      price: 12345,
-      calories: "215 Kkal",
-    },
-    {
-      key: 2,
-      image: "https://photoshopinspire.com/wp-content/uploads/2011/08/avatar_reloaded_by_saswat777.jpg",
-      name: "Kurcaci",
-      city: "Bondowoso",
-      price: 12356,
-      calories: "215 Kkal",
-    },
-    
-  ];
+  {
+    key: 1,
+    image:
+      "https://photoshopinspire.com/wp-content/uploads/2011/08/avatar_reloaded_by_saswat777.jpg",
+    name: "Belatung",
+    city: "Jember",
+    price: 12345,
+    calories: "215 Kkal",
+  },
+  {
+    key: 2,
+    image:
+      "https://photoshopinspire.com/wp-content/uploads/2011/08/avatar_reloaded_by_saswat777.jpg",
+    name: "Kurcaci",
+    city: "Bondowoso",
+    price: 12356,
+    calories: "215 Kkal",
+  },
+];
